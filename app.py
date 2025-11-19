ced92182fd611b1d208471429817bc30496830b8
hirotakasuzuki1219 <hirotaka.suzuki.1219@gmail.com>
2025-11-19 14:34:19 +1200

    Koyeb10

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def home():
 #     return render_template("map.html")
 
 # Google Search Console用の検証ファイル
-@app.route('/google486f7934b3f48a0a.html')
-def google_verification():
-    file_path = os.path.join(app.static_folder, 'google486f7934b3f48a0a.html')
-    return send_file(file_path, mimetype='text/html')
+# @app.route('/google486f7934b3f48a0a.html')
+# def google_verification():
+#     file_path = os.path.join(app.static_folder, 'google486f7934b3f48a0a.html')
+#     return send_file(file_path, mimetype='text/html')
 
 # Sitemap.xml
 @app.route('/sitemap.xml')
